f5d2796318e0dbcea0d7dbbc01055d49a79d4dff
KoychoKoychev <kgkoychev@gmail.com>
2022-04-05 14:32:30 +0300

    fix: set the production enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://parseapi.back4app.com',
+  requestHeaders: {
+    "X-Parse-Application-Id": "DrLUWya6WmD9ni2YZ2Yi1dXzGraaQiyEArZT7oop",
+    "X-Parse-REST-API-Key": "Oquz726iAu0T1Gfyrjduh6d6diipGKs10g16yAvU",
+    "X-Parse-Revocable-Session": "1",
+    "Content-Type": "application/json"
+  },
+  priceApiUrl: 'http://api.coinlayer.com/api/live?access_key=4e752aca0aae4eef10658f7ee03242d0',
 };
